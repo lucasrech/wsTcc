82e90dc6a55ae3820209a19c41c709684c3c6bea
Rech <coradin.rech@gmail.com>
2022-07-02 11:33:10 -0300

    PID da rotacao.

--- src/tccDrones/src/getTagPose.cpp
+++ src/tccDrones/src/getTagPose.cpp
@@ -6,8 +6,10 @@
 #include "ros/ros.h"
 #include "visualization_msgs/Marker.h"
 #include "visualization_msgs/MarkerArray.h"
+#include "std_msgs/Float32.h"
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/Point.h"
+#include "geometry_msgs/Quaternion.h"
 #include "ar_track_alvar_msgs/AlvarMarkers.h"
   
 using namespace std;
@@ -22,6 +24,11 @@ geometry_msgs::Point tagPoseChild1;
 geometry_msgs::Point tagPoseChild2;
 geometry_msgs::Point tagPoseChild3;
 
+geometry_msgs::Quaternion tagRotChild0;
+geometry_msgs::Quaternion tagRotChild1;
+geometry_msgs::Quaternion tagRotChild2;
+geometry_msgs::Quaternion tagRotChild3;
+
 void tagPoseCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg)
 {
   for (int i = 0; i < msg->markers.size(); i++)
@@ -30,6 +37,10 @@ void tagPoseCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg)
       tagPoseChild0.x = msg->markers[i].pose.pose.position.x;
       tagPoseChild0.y = msg->markers[i].pose.pose.position.y;
       tagPoseChild0.z = msg->markers[i].pose.pose.position.z;
+      tagRotChild0.x = msg->markers[i].pose.pose.orientation.x;
+      tagRotChild0.y = msg->markers[i].pose.pose.orientation.y;
+      tagRotChild0.z = msg->markers[i].pose.pose.orientation.z;
+      tagRotChild0.w = msg->markers[i].pose.pose.orientation.w;
     }
     if(msg->markers[i].id == TAG1){
       tagPoseChild1.x = msg->markers[i].pose.pose.position.x;
@@ -47,6 +58,18 @@ void tagPoseCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg)
       tagPoseChild3.z = msg->markers[i].pose.pose.position.z;
     }
   }
+}
+
+std_msgs::Float32 toEuler(geometry_msgs::Quaternion q)
+{
+    std_msgs::Float32 rotZRads;
+    // yaw (z-axis rotation, in rad).
+    double siny_cosp = 2 * (q.w * q.z + q.x * q.y);
+    double cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z);
+
+    rotZRads.data = std::atan2(siny_cosp, cosy_cosp);
+
+    return rotZRads;
 }
 
 int main(int argc, char **argv)
@@ -87,9 +110,11 @@ int main(int argc, char **argv)
     {
         ros::spinOnce();
 
-        ROS_INFO("pose X: %f, pose Y: %f, pose Z: %f", tagPoseChild0.x, tagPoseChild0.y, tagPoseChild0.z);
+        // ROS_INFO("pose X: %f, pose Y: %f, pose Z: %f", tagPoseChild0.x, tagPoseChild0.y, tagPoseChild0.z);
+        ROS_INFO("Rotacao: %f", toEuler(tagRotChild0).data);
 
         pubPoseChild0.publish(tagPoseChild0);
+        pubRotChild0.publish(toEuler(tagRotChild0));
         
         // ROS_WARN("...Publicado");
 
